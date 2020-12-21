b7787ce85849bf9ba243dbd2dd2dd1a5f3f463b9
yahyakansu <yahyakansusa@gmail.com>
2020-12-21 10:36:29 -0600

    Driver.class is changed

---
+++ src/test/java/pages/Test.java
@@ -0,0 +1,9 @@
+package pages;
+
+import utilities.Driver;
+
+public class Test {
+    public static void main(String[] args) {
+        Driver.getDriver().get("https://www.google.com");
+    }
+}
--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -1,45 +1,64 @@
 package utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
-public class Driver {
-    private static WebDriver driver;
-    public static WebDriver getInstance() {
-        if (driver == null) {
-            switch (Config.getProperties("browser")) {
-                case "chrome":
-                    System.setProperty("webdriver.chrome.driver", Config.getProperties("chrome.driver.path"));
-                    driver = new ChromeDriver();
-                    break;
+import java.util.concurrent.TimeUnit;
 
+public class Driver {
+    private Driver(){}
+    public static WebDriver driver;
+    public static WebDriver getDriver(){
+        if (driver==null){
+            switch (Config.getProperties("browser")){
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    chromeOptions.addArguments("--disable-notification");
+                    driver=new ChromeDriver();
+                    break;
                 case "firefox":
-                    System.setProperty("webdriver.gecko.driver", Config.getProperties("gecko.driver.path"));
+                    WebDriverManager.firefoxdriver().setup();
+                    FirefoxOptions firefoxOptions = new FirefoxOptions();
+                    firefoxOptions.addPreference("dom.webnotifications.enabled",false);
                     driver = new FirefoxDriver();
                     break;
-
-                case "safari":
-                    System.setProperty("webdriver.safari.noinstall", "true");
-                    driver = new SafariDriver();
+                case "edge":
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
                     break;
-
+                case "ie":
+                    WebDriverManager.iedriver().setup();
+                    driver = new InternetExplorerDriver();
+                    break;
                 case "opera":
-                    System.setProperty("webdriver.opera.driver",Config.getProperties("opera.driver.path"));
+                    WebDriverManager.operadriver().setup();
                     driver = new OperaDriver();
                     break;
-
-                default:
-                    System.out.println("Invalid browser type");
+                case "safari":
+                    if (System.getProperty("os.name").toLowerCase().contains("windows")){
+                        throw new WebDriverException("Windows OS does not support Safari");
+                    }
+                    WebDriverManager.getInstance(SafariDriver.class).setup();
+                    driver = new SafariDriver();
+                    break;
             }
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         }
         return driver;
     }
-
-    public static void closeDriver() {
-        if (driver != null) {
+    public static void closeDriver(){
+        if (driver!=null){
             driver.quit();
             driver=null;
         }
